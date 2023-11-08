1c30ec230c30c11451c9a5d2e01998980a85a8e8
abdulmaleekadams <harbdulmaleek@gmail.com>
2023-11-08 20:59:20 +0100

    Dictionary task

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    """
+    A function that returns a key with the biggest integer value.
+    """
+    if not a_dictionary:
+        return None
+
+    best_key = None
+    best_value = -1
+
+    for key, value in a_disctionary.items():
+        if value > best_value:
+            best_key = key
+            best_value = value
+
+    return leader
